62c308313c5ea34511dc4956118ccac172580afb
Punyakoti2001 <133564356+Punyakoti2001@users.noreply.github.com>
2024-11-13 13:37:07 +0530

    Delete microservice with different databases/account directory

--- microservice with different databases/account/src/main/java/com/example/account/customerDetailsService/CustomerDetailsService.java
+++
@@ -1,9 +0,0 @@
-package com.example.account.customerDetailsService;
-
-import com.example.account.dto.CustomerDetailesDto;
-
-public interface CustomerDetailsService 
-{
-	 CustomerDetailesDto fetchCustomerDetailes(String mobileNumber);
-
-}
